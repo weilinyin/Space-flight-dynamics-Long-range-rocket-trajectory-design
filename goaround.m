9b525743488c77101b025caa32573d84772e02f3
weilinyin <weilinyin@mail.nwpu.edu.cn>
2024-11-28 00:32:48 +0800

    Update goaround.m

--- goaround.m
+++ goaround.m
@@ -2,15 +2,15 @@ function A=goaround(a,axis)
 %a为转动角度，axis为转动轴,1,2,3代表x,y,z轴
 if axis == 1
     A=[1, 0, 0;
-       0, cosd(a), sind(a);
-       0, -sind(a), cosd(a)];
+       0, cos(a), sin(a);
+       0, -sin(a), cos(a)];
 elseif axis == 2
-    A=[cosd(a), 0, -sind(a);
+    A=[cos(a), 0, -sin(a);
        0, 1, 0;
-       sind(a), 0, cosd(a)];
+       sin(a), 0, cos(a)];
 elseif axis == 3
-    A=[cosd(a), sind(a), 0;
-       -sind(a), cosd(a), 0;
+    A=[cos(a), sin(a), 0;
+       -sin(a), cos(a), 0;
        0, 0, 1];
 end
 end
